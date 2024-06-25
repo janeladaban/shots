f10e916339ef795be54f5809fb884d75fa5b7b78
Jane Ladaban <JaneLadaban@gmail.com>
2024-06-25 17:34:04 -0400

    Initial Commit

---
+++ shots/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  shots
+//
+//  Created by Jane on 6/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ shots/shotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  shotsApp.swift
+//  shots
+//
+//  Created by Jane on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct shotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
