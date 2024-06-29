955ad8799632cbf6a54e8f394ded76d75ae9e69c
Jane Ladaban <JaneLadaban@gmail.com>
2024-06-29 18:16:50 -0400

    rename to EndView

---
+++ shots/Views/EndView.swift
@@ -0,0 +1,20 @@
+//
+//  QuestionView.swift
+//  shots
+//
+//  Created by Jane on 6/28/24.
+//
+
+import SwiftUI
+
+struct QuestionView: View {
+    var body: some View {
+        VStack(spacing: 60) {
+            Text("Hello, world!")
+        }
+    }
+}
+
+#Preview {
+    QuestionView()
+}
