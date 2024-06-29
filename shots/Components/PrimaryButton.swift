538fa88c194347b6a2b2c225ee6dba8250e2b9f1
Jane Ladaban <JaneLadaban@gmail.com>
2024-06-29 00:03:29 -0400

    add assets

---
+++ shots/Components/PrimaryButton.swift
@@ -0,0 +1,18 @@
+//
+//  PrimaryButton.swift
+//  shots
+//
+//  Created by Jane on 6/28/24.
+//
+
+import SwiftUI
+
+struct PrimaryButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PrimaryButton()
+}
---
+++ shots/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  shots
+//
+//  Created by Jane on 6/28/24.
+//
+
+import Foundation
