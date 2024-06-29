058ed9b9e4ee29c957578677cd1ba327ced1c4c7
Jane Ladaban <JaneLadaban@gmail.com>
2024-06-29 15:17:47 -0400

    rename to DataModel

---
+++ shots/Models/DataModel.swift
@@ -0,0 +1,25 @@
+//
+//  Questions.swift
+//  shots
+//
+//  Created by Jane on 6/29/24.
+//
+
+import Foundation
+
+struct QuestionData: Identifiable {
+    var id: Int
+    var question: String
+}
+
+class DataModel: NSObject {
+    static let data: [QuestionData] = [
+        QuestionData(id: 1, question: "countries you've been to"),
+        QuestionData(id: 2, question: "siblings you have"),
+        QuestionData(id: 3, question: "tattos")
+    ]
+    
+    static let testData: [QuestionData] = [
+        QuestionData(id: 0, question: "are you cute or what?!")
+    ]
+}
