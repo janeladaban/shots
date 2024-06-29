05da3d211c769eacbbb8fb84e8e5609c43d670e1
Jane Ladaban <JaneLadaban@gmail.com>
2024-06-29 00:09:06 -0400

    add Scenes folder

---
+++ shots/Scenes/Main/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  shots
+//
+//  Created by Jane on 6/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            NavigationView {
+                VStack {
+                    Image(systemName: "wineglass")
+                        .imageScale(.large)
+                        .foregroundStyle(.tint)
+                    
+                    NavigationLink(destination: QuestionsView()) {
+                        Text("Start taking shots!")
+                            .font(.title2)
+                    }
+                }
+            }
+            .navigationBarBackButtonHidden()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ shots/Scenes/Questions/QuestionsView.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionsView.swift
+//  shots
+//
+//  Created by Jane on 6/26/24.
+//
+
+import SwiftUI
+
+struct QuestionsView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+#Preview {
+    QuestionsView()
+}
