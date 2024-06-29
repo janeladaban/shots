3fbadaf0ea712b93397a438493461a129d24ee6c
Jane Ladaban <JaneLadaban@gmail.com>
2024-06-29 05:11:54 -0400

    add models

---
+++ Models/Questions.swift
@@ -0,0 +1,15 @@
+//
+//  Questions.swift
+//  shots
+//
+//  Created by Jane on 6/29/24.
+//
+
+import Foundation
+
+let questions = [
+    "countries you've been to",
+    "siblings you have",
+    "tattoos"
+]
+
