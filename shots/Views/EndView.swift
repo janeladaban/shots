a7dd11078f1e3377a48aa3b0b0e767886f5c092a
Jane Ladaban <JaneLadaban@gmail.com>
2024-07-12 21:57:26 -0400

    add GH link

--- shots/Views/EndView.swift
+++ shots/Views/EndView.swift
@@ -10,10 +10,16 @@ import SwiftUI
 struct EndView: View {
     
     var body: some View {
-        VStack {
-            Text("Thanks for playing Number of Shots!")
-                .extensionQuestion()
+        VStack(spacing: 20) {
+            Text("Thanks for playing **Number of Shots!**")
+            Text("This code lives in")
+            Link(destination: URL(string: "https://github.com/janeladaban/shots")!, 
+                 label: {
+                Text("github.com/janeladaban/shots")
+                    .underline()
+            })
         }
+        .font(.title2)
     }
 }
 
