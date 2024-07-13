a973742e7aad9d5aae9a9ef9f1fef0f3f33c6820
Jane Ladaban <JaneLadaban@gmail.com>
2024-07-12 20:14:40 -0400

    add more questions

--- shots/Models/DataModel.swift
+++ shots/Models/DataModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct QuestionData: Identifiable {
+struct QuestionData: Identifiable, Hashable {
     var id: Int
     var question: String
 }
@@ -16,7 +16,16 @@ class DataModel: NSObject {
     static let data: [QuestionData] = [
         QuestionData(id: 1, question: "countries you've been to"),
         QuestionData(id: 2, question: "siblings you have"),
-        QuestionData(id: 3, question: "tattoos you have")
+        QuestionData(id: 3, question: "NFL/MLB/NBA games you've been to"),
+        QuestionData(id: 4, question: "tattoos you have"),
+        QuestionData(id: 5, question: "languages you speak fluently"),
+        QuestionData(id: 6, question: "pets you have"),
+        QuestionData(id: 7, question: "Broadway shows you've seen live"),
+        QuestionData(id: 8, question: "cars you own"),
+        QuestionData(id: 9, question: "concerts you attended in the past year"),
+        QuestionData(id: 10, question: "Disney parks you've visited"),
+        QuestionData(id: 11, question: "marathons you've participated in"),
+        QuestionData(id: 12, question: "lotteries you won")
     ]
     
     static let testData: [QuestionData] = [
