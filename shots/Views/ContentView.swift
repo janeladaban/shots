ec36043f029ede5b98a453770b2854ffd34addf5
Jane Ladaban <JaneLadaban@gmail.com>
2024-07-12 20:19:40 -0400

    add how to play

--- shots/Views/ContentView.swift
+++ shots/Views/ContentView.swift
@@ -9,33 +9,54 @@ import SwiftUI
 
 struct ContentView: View {
     @State var data = DataModel.data
+    @State private var isShow = false
     
     var body: some View {
-        HStack {
+        VStack {
             NavigationView {
                 VStack(spacing: 40) {
+                    
+                    // title + subtitle
                     VStack(spacing: 20) {
-                        // title
                         Text("Number of Shots")
                             .extensionTitle()
                         
-                        // subtitle
                         Text("How well do you know your drinking buddies?")
                             .foregroundColor(Color("AccentColor"))
                     }
-                    // button
+                    
+                    // let's find out
                     NavigationLink {
                         ShotsView(dataQuestions: $data)
                     } label: {
                         PrimaryButton(text: "Let's find out")
                     }
                     
+                    // how to play
+                    Button("How to Play", systemImage: "info.circle") {
+                        isShow = true
+                    }
+                    // TODO: Change the background color of the sheet
+                    // TODO: Add instructions
+                    .sheet(isPresented: $isShow) {
+                        VStack(spacing: 40) {
+                            Text("How to play Number of Shots")
+                            
+                            Button("Dismiss") {
+                                isShow = false
+                            }
+                        }
+                    }
                 }
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
                 .edgesIgnoringSafeArea(.all)
                 .background(Color("BackgroundColor"))
             }
+            .navigationBarHidden(true)
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .edgesIgnoringSafeArea(.all)
+        .background(Color("BackgroundColor"))
     }
 }
 
